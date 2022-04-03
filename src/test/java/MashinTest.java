8e23a0b3ad852bf3d93842db3d3178a904e0cf67
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-04-03 18:05:43 +0430

    Objects creation

--- src/test/java/MashinTest.java
+++ src/test/java/MashinTest.java
@@ -1,14 +1,58 @@
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MashinTest {
+
+    @Test
+    void MashinBayadHarekatKonadVaghtiDarBasteAstVaMashinRoshanAst() {
+
+        Mashin benz = new Mashin("Benz");
+        Mashin volvo = new Mashin("Volvo");
+
+        boolean ayaBenzHarekatMikonad = benz.ayaDarHaleHarekatAst();
+        volvo.roshan();
+        boolean ayaVolvoHarekatMikonad = volvo.ayaDarHaleHarekatAst();
+
+        Assertions.assertFalse(ayaBenzHarekatMikonad);
+        Assertions.assertTrue(ayaVolvoHarekatMikonad);
+
+
+
+
+
+//        Mashin mashin = new Mashin();
+//        mashin.bazKardaneDar();
+//        mashin.bastaneDar();
+//
+//        System.out.println("Dar Mashin baz ast? " + mashin.ayaDareMashinBazAst);
+//
+//        mashin.harekat();
+//
+//        System.out.println("--------------------------------------");
+//
+//        Mashin pride = new Mashin("Pride");
+//        pride.roshan();
+//        pride.bazKardaneDar();
+//
+//        mashin.harekat();
+
+    }
 
     class Mashin {
 
         boolean ayaMashinRoshanAst;
         boolean ayaDareMashinBazAst;
-        double tool;
-        double arz;
         String mark;
+
+
+        Mashin() {
+            System.out.println("Yek Mashin bedune dadeh sakhte shod.");
+        }
+
+        Mashin(String mark) {
+            this.mark = mark;
+            System.out.println("Yek mashine " + this.mark + " sakhte shod.");
+        }
 
 
         void roshan() {
@@ -29,11 +73,13 @@ public class MashinTest {
             ayaDareMashinBazAst = false;
         }
 
-        void harekat() {
+        boolean ayaDarHaleHarekatAst() {
             if (!ayaDareMashinBazAst && ayaMashinRoshanAst) {
                 System.out.println("Mashin dar hale harekat ast");
+                return true;
             } else {
                 System.out.println("Mashin park ast");
+                return false;
             }
         }
 
