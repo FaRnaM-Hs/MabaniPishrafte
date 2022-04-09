8f60f6f86556ec8c8680e92cae9d884716699a35
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-04-09 18:30:55 +0430

    static class

--- src/main/java/khodro/Ranande.java
+++ src/main/java/khodro/Ranande.java
@@ -1,4 +1,29 @@
 package khodro;
 
+// Outer Class
 public class Ranande {
+
+    private String noeGavahiname = "Paye 1"; // Runtime
+    private static String nam = "Farhad"; // Compile Time
+
+
+    // Non-static Nested Class : Inner Class
+    public class Gavahiname {
+
+        public void print() {
+            System.out.println("Non Static Class: ");
+            System.out.println(noeGavahiname);
+            System.out.println(nam);
+        }
+    }
+
+    // Static Nested Class
+    public static class StaticGavahiname {
+
+        public void print() {
+            System.out.println("Static Class: ");
+            // System.out.println(noeGavahiname); --> In khat kar nemikonad
+            System.out.println(nam);
+        }
+    }
 }
---
+++ src/test/java/RanandeTest.java
@@ -0,0 +1,18 @@
+import khodro.Ranande;
+import org.junit.jupiter.api.Test;
+
+public class RanandeTest {
+
+    @Test
+    void BayadNoeGavahinameVaNamRaChapKonad() {
+        Ranande ranande = new Ranande();
+        Ranande.Gavahiname gavahiname = ranande.new Gavahiname();
+        gavahiname.print();
+    }
+
+    @Test
+    void BayadNamRaChapKonad() {
+        Ranande.StaticGavahiname staticGavahiname = new Ranande.StaticGavahiname();
+        staticGavahiname.print();
+    }
+}
