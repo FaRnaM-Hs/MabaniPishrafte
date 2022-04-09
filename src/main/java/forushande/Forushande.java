f0528155eb54db4fbc4ddde2153a1bc337a796ca
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-04-09 18:05:57 +0430

    static block

--- src/main/java/forushande/Forushande.java
+++ src/main/java/forushande/Forushande.java
@@ -4,6 +4,13 @@ import khodro.jadid.Mashin;
 
 public class Forushande {
 
+    public static String[] nameForushandegan = new String[10];
+
+    static {
+        nameForushandegan[0] = "Ali";
+        nameForushandegan[1] = "Ahmad";
+    }
+
     public void forushMashin() {
         Mashin.tedadeForush++;
     }
