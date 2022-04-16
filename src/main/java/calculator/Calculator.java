f3be29c6f6dc91ae3a6d3c02895f59c90e271376
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-04-16 20:35:05 +0430

    final variables

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,31 +1,31 @@
 package calculator;
 
 public class Calculator {
-    public String add(String adadAval, String adadDovvom) {
-        double d1 = Double.parseDouble(adadAval);
-        double d2 = Double.parseDouble(adadDovvom);
-        double result = d1 + d2;
+    public String add(final String adadAval, final String adadDovvom) {
+        final double d1 = Double.parseDouble(adadAval);
+        final double d2 = Double.parseDouble(adadDovvom);
+        final double result = d1 + d2;
         return String.valueOf(result);
     }
 
-    public String submission(String adadAval, String adadDovvom) {
-        double d1 = Double.parseDouble(adadAval);
-        double d2 = Double.parseDouble(adadDovvom);
-        double result = d1 - d2;
+    public String submission(final String adadAval, final String adadDovvom) {
+        final double d1 = Double.parseDouble(adadAval);
+        final double d2 = Double.parseDouble(adadDovvom);
+        final double result = d1 - d2;
         return String.valueOf(result);
     }
 
-    public String multiply(String adadAval, String adadDovvom) {
-        double d1 = Double.parseDouble(adadAval);
-        double d2 = Double.parseDouble(adadDovvom);
-        double result = d1 * d2;
+    public String multiply(final String adadAval, final String adadDovvom) {
+        final double d1 = Double.parseDouble(adadAval);
+        final double d2 = Double.parseDouble(adadDovvom);
+        final double result = d1 * d2;
         return String.valueOf(result);
     }
 
-    public String division(String adadAval, String adadDovvom) {
-        double d1 = Double.parseDouble(adadAval);
-        double d2 = Double.parseDouble(adadDovvom);
-        double result = d1 / d2;
+    public String division(final String adadAval, final String adadDovvom) {
+        final double d1 = Double.parseDouble(adadAval);
+        final double d2 = Double.parseDouble(adadDovvom);
+        final double result = d1 / d2;
         return String.valueOf(result);
     }
 }
