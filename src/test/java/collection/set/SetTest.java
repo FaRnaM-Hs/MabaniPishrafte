61572f7307ce29fe26a23c9d22e704d55c510a03
FaRnaM-Hs <farnam.hs@yahoo.com>
2022-04-27 15:21:18 +0430

    TreeSet

--- src/test/java/collection/set/SetTest.java
+++ src/test/java/collection/set/SetTest.java
@@ -8,7 +8,7 @@ import java.util.*;
 public class SetTest {
 
     @Test
-    void Should_Remove_Duplication() {
+    void HashSet_Should_Remove_Duplication() {
         Set<String> animals = new HashSet<>();
         animals.add("Dog");
         animals.add("Cat");
@@ -22,15 +22,27 @@ public class SetTest {
         exceptedAnimals.add("Rabbit");
 
         Assertions.assertThat(animals).isEqualTo(exceptedAnimals);
-        
+
         for (String animal : animals) {
             System.out.println(animal);
         }
     }
 
     @Test
-    void Should_Maintain_Insertion_Order() {
+    void LinkedHashSet_Should_Maintain_Insertion_Order() {
         Set<String> animals = new LinkedHashSet<>();
+        animals.add("Dog");
+        animals.add("Cat");
+        animals.add("Rabbit");
+
+        for (String animal : animals) {
+            System.out.println(animal);
+        }
+    }
+
+    @Test
+    void TreeSet_Should_Sort_Data() {
+        Set<String> animals = new TreeSet<>();
         animals.add("Dog");
         animals.add("Cat");
         animals.add("Rabbit");
